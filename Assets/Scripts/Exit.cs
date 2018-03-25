225c10b2e7c327573590d771638ad13a94cf5ac1
deadairspace <12399921+deadairspace@users.noreply.github.com>
2018-03-25 01:49:34 -0600

    Room almost Done, Char not Moving!

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Serializable: Lets you embed a class with sub
+properties in the inspector you can use this to 
+display variable in the inspactor similar to how a 
+vector 3 shows up in the inspector.*/
+
+[System.Serializable]
+public class Exit
+{
+    public string keyString;
+    public string exitDescription;
+    public Room valueRoom;
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MusicAwareness/Room")]
+public class Room : ScriptableObject
+{
+    public string description;
+    public string roomName;
+    public Exit[] exits;
+
+
+}
